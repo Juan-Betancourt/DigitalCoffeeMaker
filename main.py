04f6306a85b7663de4fcabc1f6cb2a662e86c855
Juan Carlos Betancourt <juancbetancourt11@gmail.com>
2022-06-20 22:59:10 -0400

    Initial TODO list for Digital Coffee Maker

---
+++ main.py
@@ -0,0 +1,13 @@
+#TODO: 1. Prompt User Question
+
+#TODO: 2. Enter while loop to turn on and off machine
+
+#TODO: 3. Print Report
+
+#TODO: 4. Check sufficient Resources
+
+#TODO: 5. Process Coins
+
+#TODO: 6. Check if Transaction was Successful
+
+#TODO: 7. Make Coffee for the Customer
