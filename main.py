0f3de09f9b665da822de1a4812745b32401abefc
Juan Carlos Betancourt <juancbetancourt11@gmail.com>
2022-06-21 21:22:57 -0400

    Start code

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+from menu import MENU, resources
+
 #TODO: 1. Prompt User Question
+
+user_coffee_selection = input("What type of coffee would you like? (espresso | latte | cappuccino): ").lower()
 
 #TODO: 2. Enter while loop to turn on and off machine
 
+coffee_machine = "on"
+
 #TODO: 3. Print Report
+
+
 
 #TODO: 4. Check sufficient Resources
 
 #TODO: 5. Process Coins
+
+def __process_coins__():
+    """Returns total calculated from coins inserted"""
+    print("Please insert coins")
+    quarters = int(input("How many quarters? "))
+    dimes
 
 #TODO: 6. Check if Transaction was Successful
 
